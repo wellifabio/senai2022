13c7411f8abf8858db48e0d5f417051ccd700516
wellifabio <wellifabio@gmail.com>
2022-02-21 11:09:27 -0300

    acumula

--- 1des/fpoo/aula10/Acumula.java
+++ 1des/fpoo/aula10/Acumula.java
@@ -8,27 +8,34 @@ public class Acumula {
 	public static void main(String[] args) {
 		System.out.println("O algoritmo de acumulação");
 		System.out.print("simples, retornou = ");
-		System.out.println(simples(345));
-		System.out.println("Total = " + aleatorio(345));
-	}
-	
+		System.out.println(simples(5));
+		System.out.println("Total = " + aleatorio(5));
+		System.out.println("5!="+fatorial(5));
+	}	
 	public static int simples(int iteracoes) {
 		int x  = 0;
 		for(int i = 0; i < iteracoes; i++)
 			x+=i;
 		return x;
 	}
-	
 	public static int aleatorio(int iteracoes) {
 		rand = new Random();
 		int x = 0;
 		int r = 0;
 		for(int i = 0; i < iteracoes; i++) {
 			r = rand.nextInt(100);
-			System.out.println("Aleatorio = " + r);
 			x+=r;
-			System.out.println("Acumulado = " + x);
+			System.out.print("Aleatorio = " + r);
+			System.out.println("\tAcumulado = " + x);
 		}
 		return x;
 	}
+	public static int fatorial(int n) {
+		int x  = 1;
+		for(int i = n; i > 0; i--)
+			x*=i;
+		return x;
+	}
+	
+
 }
