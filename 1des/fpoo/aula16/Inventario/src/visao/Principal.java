86e0617dc5713f9de7b5c58f94d8c2ebd9886184
wellifabio <wellifabio@gmail.com>
2022-03-11 11:07:49 -0300

    fpoo aula16

--- 1des/fpoo/aula16/Inventario/src/visao/Principal.java
+++ 1des/fpoo/aula16/Inventario/src/visao/Principal.java
@@ -9,6 +9,7 @@ import modelo.Item;
 public class Principal {
 
 	static Scanner s = new Scanner(System.in);
+	//Atributo para formatar a Data em dia / mês / ano
 	static SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
 
 	public static void main(String[] args) throws IOException, ParseException {
@@ -16,22 +17,24 @@ public class Principal {
 		System.out.println("Cálculo de inventário");
 		System.out.print("Digite quantos ítens deseja colocar no inventário:");
 		int qtdInstancias = s.nextInt();
-		Item[] itens = new Item[qtdInstancias]; //Instância
+		Item[] itens = new Item[qtdInstancias]; //Instâncias
 	
 		System.out.println("Digite o "+qtdInstancias+" itens\nNome\tLocal\tValor\tData da Compra");
 		for (int i = 0; i < qtdInstancias; i++) {
-			itens[i] = new Item();//Objeto
+			itens[i] = new Item();//Objeto único
 			itens[i].nome = s.next();
 			itens[i].local = s.next();
 			itens[i].valor = s.nextDouble();
-			itens[i].dataCompra = df.parse(s.next());
+			itens[i].dataCompra = df.parse(s.next()); //Converte String para Data
 		}
 
 		System.out.println("Nome\tLocal\tValor\tData Compra\tAnos");
 		for (int i = 0; i < qtdInstancias; i++) {
+			//Utiliza o método para string
 			System.out.println(itens[i].paraString());
 		}
 		
+		//Algoritmo de acumulação para somar o total
 		double total = 0;
 		for (int i = 0; i < qtdInstancias; i++) {
 			total += itens[i].valor;
