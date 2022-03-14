2f6b20df6aa7d26ed83fd579884f3b0550c0eab9
wellifabio <wellifabio@gmail.com>
2022-03-14 11:28:02 -0300

    aula16 exemplo

---
+++ 1des/fpoo/aula16/BanhoETosa/src/visao/Menu.java
@@ -0,0 +1,17 @@
+package visao;
+
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+public class Menu {
+
+	private static Scanner read = new Scanner(System.in);
+	private static SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+	private static int opcao = 0;
+	private static String funcionalidades[] = {"Cadastrar Pet","Cadastrar Servico"};
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
