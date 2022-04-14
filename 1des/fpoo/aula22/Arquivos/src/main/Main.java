ac4814bff646f5d9693729fe7c2398feaff3f6f7
wellifabio <wellifabio@gmail.com>
2022-04-14 10:46:11 -0300

    File

--- 1des/fpoo/aula22/Arquivos/src/main/Main.java
+++ 1des/fpoo/aula22/Arquivos/src/main/Main.java
@@ -17,6 +17,7 @@ public class Main {
 			System.out.println("2 - Listar;");
 			System.out.println("0 - Sair;");
 			opcao = input.nextInt();
+			input.nextLine();
 			
 			switch(opcao) {
 				case 1: 
@@ -36,8 +37,18 @@ public class Main {
 				case 2:
 					ArrayList<String> infoFile = arq.ler("Pessoas");
 					
+					System.out.println("Informe o nome que deseja localizar : ");
+					String busca = input.nextLine();
+					
+					// 0     1      2
+					//nome;idade;cidade
 					for(String linha : infoFile) {
-						System.out.println(linha);
+						String[] temp = linha.split(";");
+						if(temp[2].toLowerCase().contains(busca.toLowerCase())) {
+							System.out.println("Nome : " + temp[0]);
+							System.out.println("Idade : " + temp[1]);
+							System.out.println("Cidade : " + temp[2]);	
+						}
 					}
 					break;
 				case 0:
