173c43ec69e7b80a622107b0434d6240c1d24303
wellifabio <wellifabio@gmail.com>
2022-04-25 11:11:14 -0300

    fpoo aula23

--- 1des/fpoo/aula23/SomaArquivos/src/solucao/Principal.java
+++ 1des/fpoo/aula23/SomaArquivos/src/solucao/Principal.java
@@ -1,17 +1,22 @@
 package solucao;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Principal {
 
-	// Espaço de memória dinâmico
+	// Espaço de memória dinâmico para leitura(Abrir)
 	static BufferedReader br;
+	// Espaço de memória dinâmico para escrita(Salvar)
+	static BufferedWriter bw;
 	// Endereçp do aquivo de entrada
 	static String entrada = "C:\\Users\\Aluno.DSN5131112167\\Desktop\\work_arquivos\\SomaArquivos\\arquivos\\entrada.csv";
+	static String saida = "C:\\Users\\Aluno.DSN5131112167\\Desktop\\work_arquivos\\SomaArquivos\\arquivos\\saida.csv";
 	static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
@@ -25,6 +30,7 @@ public class Principal {
 			try {
 				// Abre o arquivo
 				br = new BufferedReader(new FileReader(entrada));
+				bw = new BufferedWriter(new FileWriter(saida,false));
 				String linha = br.readLine();// Lê a primeira linha
 				while (linha != null) {
 					String[] vetor = linha.split(";");
@@ -41,10 +47,12 @@ public class Principal {
 						result = v1 / v2;
 					}
 					System.out.println(result);
+					bw.write(result+"\r\n");
 					linha = br.readLine();// Lê a próxima linha
 				}
 				// Fecha o arquivo
 				br.close();
+				bw.close();
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
@@ -53,5 +61,4 @@ public class Principal {
 		}
 		System.out.println("Adeus.");
 	}
-
 }
