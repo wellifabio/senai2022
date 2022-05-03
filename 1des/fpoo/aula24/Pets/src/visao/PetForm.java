038dd5fad6554da8a465a2c41372bc01bb29b08c
wellifabio <wellifabio@gmail.com>
2022-05-03 08:40:15 -0300

    fpoo aula24

--- 1des/fpoo/aula24/Pets/src/visao/PetForm.java
+++ 1des/fpoo/aula24/Pets/src/visao/PetForm.java
@@ -23,17 +23,18 @@ public class PetForm extends JFrame implements ActionListener {
 	private JComboBox<String> cbEspecie;
 	private JTextArea verResultados;
 	private JButton create, read, update, delete;
-	private String imgIco = "C:\\Users\\Professor\\Desktop\\work\\Pets\\assets\\icone.png";
-	private String[] imagens = { "C:\\Users\\Professor\\Desktop\\work\\Pets\\assets\\doguinho.png",
-			"C:\\Users\\Professor\\Desktop\\work\\Pets\\assets\\miau.png",
-			"C:\\Users\\Professor\\Desktop\\work\\Pets\\assets\\coelho.png",
-			"C:\\Users\\Professor\\Desktop\\work\\Pets\\assets\\ornitorrinco.png" };
+	private String imgIco = "C:\\Users\\Aluno.DSN5131112167\\Desktop\\gui\\Pets\\assets\\icone.png";
+	private String[] imagens = { "C:\\Users\\Aluno.DSN5131112167\\Desktop\\gui\\Pets\\assets\\doguinho.png",
+			"C:\\Users\\Aluno.DSN5131112167\\Desktop\\gui\\Pets\\assets\\miau.png",
+			"C:\\Users\\Aluno.DSN5131112167\\Desktop\\gui\\Pets\\assets\\coelho.png",
+			"C:\\Users\\Aluno.DSN5131112167\\Desktop\\gui\\Pets\\assets\\ornitorrinco.png" };
 	private ImageIcon icon;
 	private int autoId = 1;
+	private String saida = "";
 
 	PetForm() {
 		setTitle("Formulário de Pets");
-		setBounds(200, 300, 500, 500);
+		setBounds(200, 300, 600, 500);
 		setIconImage(new ImageIcon(imgIco).getImage());
 		painel = new JPanel();
 		painel.setBackground(new Color(255, 233, 213));
@@ -65,54 +66,55 @@ public class PetForm extends JFrame implements ActionListener {
 		telefone = new JLabel("Telefone:");
 		telefone.setBounds(20, 230, 120, 30);
 		painel.add(telefone);
-		rotulos = new JLabel("Id:     Espécie:       Pet:        Peso:       Idade:       Dono:        Telefone:");
-		rotulos.setBounds(20, 260, 400, 30);
+		rotulos = new JLabel(
+				"Id:...Espécie:.......Pet:.....................Peso:...................Idade:..................Dono:..................Telefone:");
+		rotulos.setBounds(20, 260, 500, 30);
 		painel.add(rotulos);
 
 		tfId = new JTextField(String.format("%d", autoId));
 		tfId.setEditable(false);
-		tfId.setBounds(140, 20, 160, 30);
+		tfId.setBounds(140, 20, 260, 30);
 		painel.add(tfId);
-		cbEspecie = new JComboBox<String>(new String[] {"Cachorro", "Gato", "Coelho", "Outro" });
-		cbEspecie.setBounds(140, 50, 160, 30);
+		cbEspecie = new JComboBox<String>(new String[] { "Cachorro", "Gato", "Coelho", "Outro" });
+		cbEspecie.setBounds(140, 50, 260, 30);
 		painel.add(cbEspecie);
 		tfNomePet = new JTextField();
-		tfNomePet.setBounds(140, 80, 160, 30);
+		tfNomePet.setBounds(140, 80, 260, 30);
 		painel.add(tfNomePet);
 		tfRaca = new JTextField();
-		tfRaca.setBounds(140, 110, 160, 30);
+		tfRaca.setBounds(140, 110, 260, 30);
 		painel.add(tfRaca);
 		tfPeso = new JTextField();
-		tfPeso.setBounds(140, 140, 160, 30);
+		tfPeso.setBounds(140, 140, 260, 30);
 		painel.add(tfPeso);
 		tfNascimento = new JTextField();
-		tfNascimento.setBounds(140, 170, 160, 30);
+		tfNascimento.setBounds(140, 170, 260, 30);
 		painel.add(tfNascimento);
 		tfNomeDono = new JTextField();
-		tfNomeDono.setBounds(140, 200, 160, 30);
+		tfNomeDono.setBounds(140, 200, 260, 30);
 		painel.add(tfNomeDono);
 		tfTelefone = new JTextField();
-		tfTelefone.setBounds(140, 230, 160, 30);
+		tfTelefone.setBounds(140, 230, 260, 30);
 		painel.add(tfTelefone);
 		verResultados = new JTextArea();
 		verResultados.setEnabled(false);
-		verResultados.setBounds(20, 290, 445, 150);
+		verResultados.setBounds(20, 290, 545, 150);
 		verResultados.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
 		painel.add(verResultados);
 		imagem = new JLabel();
-		imagem.setBounds(310, 145, 150, 115);
+		imagem.setBounds(410, 145, 150, 115);
 		imagem.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
-		mostrarImagem(0);
+		alternarImagens(0);
 		painel.add(imagem);
 
 		create = new JButton("Cadastrar");
 		read = new JButton("Buscar");
 		update = new JButton("Atualizar");
 		delete = new JButton("Excluir");
-		create.setBounds(310, 20, 150, 30);
-		read.setBounds(310, 50, 150, 30);
-		update.setBounds(310, 80, 150, 30);
-		delete.setBounds(310, 110, 150, 30);
+		create.setBounds(410, 20, 150, 30);
+		read.setBounds(410, 50, 150, 30);
+		update.setBounds(410, 80, 150, 30);
+		delete.setBounds(410, 110, 150, 30);
 		update.setEnabled(false);
 		delete.setEnabled(false);
 		painel.add(create);
@@ -128,7 +130,7 @@ public class PetForm extends JFrame implements ActionListener {
 
 	}
 
-	private void mostrarImagem(int indice) {
+	private void alternarImagens(int indice) {
 		icon = new ImageIcon(new ImageIcon(imagens[indice]).getImage().getScaledInstance(150, 115, 100));
 		imagem.setIcon(icon);
 	}
@@ -136,7 +138,7 @@ public class PetForm extends JFrame implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == cbEspecie) {
-			mostrarImagem(cbEspecie.getSelectedIndex());
+			alternarImagens(cbEspecie.getSelectedIndex());
 		}
 	}
 
