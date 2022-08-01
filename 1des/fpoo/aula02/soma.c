9a4acabaa97c72648406a80185c56508add55258
wellifabio <wellifabio@hotmail.com>
2022-08-01 10:41:46 -0300

    1des fpoo aula2

---
+++ 1des/fpoo/aula02/soma.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int a,b,c,d;
+	printf("Digite um número inteiro: ");
+	scanf("%d",&a);
+	printf("Digite outro número inteiro: ");
+	scanf("%d",&b);
+	printf("Digite outro número inteiro: ");
+	scanf("%d",&c);
+	d = a + b + c;
+	printf("A soma dos três números é %d",d);	
+}
+
