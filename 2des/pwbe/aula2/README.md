e27c89bd7930c1d4c0cc711d29f4be8c34bcb5e1
Reenye Lima <reenyelima@hotmail.com>
2022-08-05 11:05:49 -0300

    2des pwbe aula02

--- 2des/pwbe/aula2/README.md
+++ 2des/pwbe/aula2/README.md
@@ -1,4 +1,8 @@
 # Back-End Node JS
 - Iniciar um novo projeto "Pedidos"
-- Podemos utilizar a dependência nodemom para que o servidor seja reiniciado automaticamente ao salvar qualquer arquivo
-- Vamos aprender a enviar dados para o servidor utilizando o verbo GET
+	- Podemos utilizar a dependência nodemom para que o servidor seja reiniciado automaticamente ao salvar qualquer arquivo
+	- Vamos aprender a enviar dados para o servidor utilizando o verbo GET
+	- Para isso criamos um formulário chamado "clientes.html" que está na pasta frontpedidos
+	- Enviamos os dados para a url local do backend "http://localhost:3000/pedidos"
+	- Criamos um banco de dados utilizando o script da aula02 de banco de dados (bcd)
+	- Importamos a dependência mysql -> npm instal mysql e programamos a conexão e a resposta ao request.
--- 2des/pwbe/aula2/pedidos/index.js
+++ 2des/pwbe/aula2/pedidos/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
+
+const con = mysql.createConnection({
+    user : 'root',
+    host : 'localhost',
+    database : 'pedidos'
+});
 
 app.use(express.json());
 
 app.get('/pedidos',(req, res)=>{
+    let priNome = req.query.priNome;
+    let sobrenome = req.query.sobrenome;
+    let endereco = req.query.endereco;
+    let string = `insert into clientes value(null,'${priNome}','${sobrenome}','${endereco}')`;
     console.log(req.query);
-    res.json("OK");
+    con.query(string,(err,result)=>{
+        if(err == null){
+            res.json("Dados recebidos com sucesso e enviados para o nosso Banco de Dados");
+        } else {
+            res.json("Dados recebidos com sucesso, porém não conseguimos enviar para o banco de dados");
+        }
+    });
 });
 
 app.listen(3000,()=>{
