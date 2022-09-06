0df0d8c337e983a6439dd9fff5001ff6f02790b8
wellifabio <wellifabio@hotmail.com>
2022-09-06 08:12:20 -0300

    1des fpoo aula07

--- 1des/fpoo/aula07/correcao/ex1.c
+++ 1des/fpoo/aula07/correcao/ex1.c
@@ -2,14 +2,30 @@
 #include<locale.h>
 int main(){
 	setlocale(LC_ALL,"");
-	char nomes[5][20];
+	char nomes[5][10];
 	float salarios[5];
+	float percentual;
 	int i;
 	
-	//Entrada
-	printf("Digite os nomes dos 5 funcionários:");
+	//Entradas
+	printf("Digite os nomes dos 5 funcionários:\n");
 	for(i = 0; i < 5; i++){
-		scanf("%s",nomes[i]);
+		scanf("%s",&nomes[i]);
 	}
+	printf("Digite os 5 salários respectivamente:\n");
+	for(i = 0; i < 5; i++){
+		do{
+			scanf("%f",&salarios[i]);
+		}while(salarios[i] < 0 || salarios[i]>1000000);
+	}
+	printf("Digite percentual(%%) de Reajuste:\n");
+	do{
+		scanf("%f",&percentual);
+	}while(percentual < 0 || percentual>1000);
 	
+	//Procesamento e Saída
+	for(i = 0; i < 5; i++){
+		salarios[i] = salarios[i] * percentual / 100 + salarios[i];
+		printf("%s novo salário R$%.2f\n",nomes[i],salarios[i]);
+	}
 }
