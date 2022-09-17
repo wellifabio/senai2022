dc12426d9e32a10ed7359bb41219360b58cbc064
Wellington Fábio de Oliveira Martins <44377232+wellifabio@users.noreply.github.com>
2022-09-17 12:03:38 -0300

    Update script_sql_01.sql

--- dp900_01/aula_sql/script_sql_01.sql
+++ dp900_01/aula_sql/script_sql_01.sql
@@ -33,3 +33,44 @@ insert into CLIDP01 values
 
 -- Consultar os dados da tabela
 select * from clidp01;
+select nome from clidp01;
+select cod_cli, nome from clidp01;
+
+-- Alterar registros
+update clidp01 set nome = 'Ana' where cod_cli = 1;
+
+-- excluir
+delete from clidp01 where cod_cli = 1;
+
+-- Cria novamente as tabelas normalizadas 1º Forma normal.
+create table clidp02(
+    id_cli number not null primary key,
+    pri_nome varchar(30) not null,
+    sobrenome varchar(30) not null,
+    cep varchar(8) not null,
+    numero varchar(5),
+    complemento varchar(30)
+);
+create table telefonesdp01(
+    id_cli number not null,
+    tipo varchar(10) not null,
+    numero varchar(15) not null,
+    foreign key (id_cli) references clidp02(id_cli)
+);
+
+describe clidp02;
+describe telefonesdp01;
+
+insert into clidp02 values(1,'Ana','Sousa','13914456','15','');
+insert into clidp02 values(2,'Lúcia','Silva','13914452','43B','bl15 ap44');
+insert into clidp02 values(3,'Mariana','Melo','13914426','23A','fundos');
+insert into clidp02 values(4,'Silvia','Oliveira','13914413','314','');
+
+insert into telefonesdp01 values(1,'celular','(19)99778-8888');
+insert into telefonesdp01 values(1,'fixo','(19)99778-7777');
+insert into telefonesdp01 values(2,'celular','(19)99778-6666');
+insert into telefonesdp01 values(2,'comercial','(19)99778-5555');
+insert into telefonesdp01 values(3,'celular','(19)99778-4444');
+insert into telefonesdp01 values(3,'fixo','(19)99778-3333');
+insert into telefonesdp01 values(3,'comercial','(19)99778-2222');
+insert into telefonesdp01 values(5,'celular','(19)99778-1111');
