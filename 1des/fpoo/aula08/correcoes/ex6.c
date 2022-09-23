65c1e72e56374b37b7319b5df9450ed27c6a4807
ReenyeLima <reenyelima@hotmail.com>
2022-09-23 09:05:15 -0300

    1des fpoo aula08

---
+++ 1des/fpoo/aula08/correcoes/ex6.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<locale.h>
+void retangulo(int x, int y, char c);
+int main(){
+	setlocale(LC_ALL,"");
+	int l, a;
+	char c;
+	printf("Digite a largura, altura e caracter do retângulo:\n");
+	scanf("%d",&l);
+	scanf("%d",&a);
+	c = getch();
+	retangulo(l, a, c);
+}
+//Função que desenha um retângulo
+void retangulo(int x, int y, char c){
+	int i, j;
+	for(i = 0; i < y; i++){
+		for(j = 0; j < y; j++)
+			printf("%c",c);
+		printf("\n");
+	}
+}
