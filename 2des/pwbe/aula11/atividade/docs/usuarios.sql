64bf3689a3c077e61bb357705dbbbe05cf3fee99
Reenye Lima <reenyelima@hotmail.com>
2022-10-20 11:03:41 -0300

    diretorio reorganizado

--- 2des/pwbe/aula11/atividade/docs/usuarios.sql
+++ 2des/pwbe/aula11/atividade/docs/usuarios.sql
@@ -13,6 +13,6 @@ create table usuarios(
 insert into usuarios values
 ("adrianalemos","Adriana Batista Lemos","2005-03-25","(19)98787-7778",to_base64(LOAD_FILE("D:/wellington/senai2022/2des/pwbe/aula11/atividade/assets/avatar01.png"))),
 ("mauricioduarte","Mauricio Duarte","1998-01-20","(19)98787-33333",to_base64(LOAD_FILE("D:/wellington/senai2022/2des/pwbe/aula11/atividade/assets/avatar02.png"))),
-("anamello","Ana Maria Mello","2008-08-22","(19)98888-7661",to_base64(OAD_FILE("D:/wellington/senai2022/2des/pwbe/aula11/atividade/assets/avatar03.png")to_base64();
+("anamello","Ana Maria Mello","2008-08-22","(19)98888-7661",to_base64(LOAD_FILE("D:/wellington/senai2022/2des/pwbe/aula11/atividade/assets/avatar03.png")));
 
 select login, nome, nascimento, telefone from usuarios;
