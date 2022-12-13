979189d632cf1d5a2cfc4bb535a13353a4cbd8dd
Reenye Lima <reenyelima@hotmail.com>
2022-12-13 08:35:10 -0300

    recuperacao

---
+++ 2des/pwfe/aula13/script.sql
@@ -0,0 +1,24 @@
+drop database if exists chamados;
+create database chamados charset=UTF8 collate utf8_general_ci;
+use chamados;
+
+create table chamados(
+	id integer not null primary key auto_increment,
+	tipo varchar(50) not null,
+	severidade integer(1) not null,
+	descricao varchar(256) not null,
+	data date not null,
+	hora time not null,
+	hora_inicio time,
+	hora_fim time,
+	destino varchar(50) not null
+);
+
+LOAD DATA INFILE 'D:/wellington/senai2022/2des/bcd/aula04/telefones.CSV'
+INTO TABLE chamados
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+select * from chamados;
